51d2aea2aae8f296567f73200a88339252c6647b
Sylvain Lasarte <sylvainlasarte@gmai.com>
2018-10-16 10:11:52 +0200

    Ajout footer modification css Ajout heure

--- script/script.js
+++ script/script.js
@@ -1,11 +1,45 @@
+//mode nuit
 function changeBackground() {
 	if (document.getElementById("mode").value == "Mode nuit"){
-   		document.body.style.background = "#000000";
-   		document.body.style.color = "#FFFFFF";
+   		document.body.style.background = "#282923";
+   		document.body.style.color = "#F5F5F5";
    		document.getElementById("mode").value = "Mode jour";
    }else {
-   		document.body.style.background = "#FFFFFF";
-   		document.body.style.color = "#000000"; 
+   		document.body.style.background = "#F5F5F5";
+   		document.body.style.color = "#282923"; 
    		document.getElementById("mode").value = "Mode nuit";
    }
 }
+
+window.onload = function onload(){
+	
+	//afficher l'heure
+	horloge('heure');
+
+	//initialiser ladate
+	var ladate = new Date();
+
+	//mode nuit quand c'est la nuit
+	if (ladate.getHours()>20 && ladate.getHours()<6)
+	{
+		document.body.style.background = "#282923";
+   		document.body.style.color = "#F5F5F5";
+   		document.getElementById("mode").value = "Mode jour";
+   		document.getElementById("intro").innerHTML = "Bonsoir et bienvenue sur le portfolio de Sylvain Lasarte. ";
+	}
+
+//afficher l'heure
+function horloge(el) {
+  if(typeof el=="string") { el = document.getElementById(el); }
+  function actualiser() {
+    var date = new Date();
+    var str = date.getHours();
+    str += ':'+(date.getMinutes()<10?'0':'')+date.getMinutes();
+    str += ':'+(date.getSeconds()<10?'0':'')+date.getSeconds();
+    el.innerHTML = str;
+  }
+  actualiser();
+  setInterval(actualiser,1000);
+}
+};
+
